c09a8e1fb71fc81c7b9415ec8c7610a4949733dd
alianmc1988 <alianmc1988@gmail.com>
2021-05-16 18:59:21 -0400

    almost done

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DemoComponent } from './demo/demo.component';
+// Instalar Angular Animations
+
+
+@NgModule({
+  declarations: [PortfolioComponent, AboutComponent, ContactComponent, LandingpageComponent, NavbarComponent, FooterComponent, DemoComponent],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/landingpage/landingpage.component.ts
@@ -0,0 +1,46 @@
+import { state, style, trigger,transition,animation, animate } from '@angular/animations';
+import { NONE_TYPE } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage',
+  templateUrl: './landingpage.component.html',
+  styleUrls: ['./landingpage.component.css'],
+  animations:[
+    trigger('initState',[
+      state('void',style({
+        transform:'translateX(-200%)',
+        opacity: 0
+      })),
+      transition (':enter',[
+        animate (1000,style({
+         transform: 'translateX(0)',
+          opacity:1
+          
+        }))
+      ])
+    ]),
+    trigger('initStateBrand',[
+      state('void',style({
+        // transform:'translateX(20%)',
+        
+        opacity: 0
+      })),
+      transition (':enter',[
+        animate (1500,style({
+          // transform: 'translateX(0)',
+          opacity:1
+          
+        }))
+      ])
+    ])
+  ]
+})
+export class LandingpageComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+  
+}
